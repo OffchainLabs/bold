137a5f3a1cba2d5e418beb22c6ceac71ff565921
rauljordan <raul@prysmaticlabs.com>
2022-12-08 00:33:37 -0500

    three test failing

--- validator/challenges_test.go
+++ validator/challenges_test.go
@@ -92,6 +92,65 @@ func TestBlockChallenge(t *testing.T) {
 		}
 		runBlockChallengeTest(t, cfg)
 	})
+	t.Run("three validators opening leaves at same height", func(t *testing.T) {
+		aliceAddr := common.BytesToAddress([]byte("a"))
+		bobAddr := common.BytesToAddress([]byte("b"))
+		charlieAddr := common.BytesToAddress([]byte("c"))
+		cfg := &blockChallengeTestConfig{
+			numValidators:  3,
+			numStateRoots:  6,
+			validatorAddrs: []common.Address{aliceAddr, bobAddr, charlieAddr},
+			validatorNamesByAddress: map[common.Address]string{
+				aliceAddr:   "alice",
+				bobAddr:     "bob",
+				charlieAddr: "charlie",
+			},
+			// The heights at which the validators diverge in histories. In this test,
+			// alice and bob agree up to and including height 3.
+			divergenceHeightsByAddress: map[common.Address]uint64{
+				aliceAddr:   3,
+				bobAddr:     3,
+				charlieAddr: 3,
+			},
+		}
+		// NOTE: THIS SHOULD FAIL!
+		cfg.eventsToAssert = []protocol.ChallengeEvent{
+			// Alice adds a challenge leaf 6, is presumptive.
+			&protocol.ChallengeLeafEvent{
+				Validator: aliceAddr,
+			},
+			// Bob adds leaf 6.
+			&protocol.ChallengeLeafEvent{
+				Validator: bobAddr,
+			},
+			// Bob bisects to 4, is presumptive.
+			&protocol.ChallengeBisectEvent{
+				Validator: bobAddr,
+			},
+			// Alice bisects to 4.
+			&protocol.ChallengeBisectEvent{
+				Validator: aliceAddr,
+			},
+			// Alice bisects to 2, is presumptive.
+			&protocol.ChallengeBisectEvent{
+				Validator: aliceAddr,
+			},
+			// Bob merges to 2.
+			&protocol.ChallengeMergeEvent{
+				Validator: bobAddr,
+			},
+			// Bob bisects from 4 to 3, is presumptive.
+			&protocol.ChallengeBisectEvent{
+				Validator: bobAddr,
+			},
+			// Alice merges to 3.
+			&protocol.ChallengeMergeEvent{
+				Validator: aliceAddr,
+			},
+			// Both challengers are now at a one-step fork, we now await subchallenge resolution.
+		}
+		runBlockChallengeTest(t, cfg)
+	})
 	t.Run("two validators opening leaves at very different heights", func(t *testing.T) {
 		// TODO: Needs to change the state roots each validator has. Bob might have up to height N
 		// while Alice has up to height M, which will cause them to post leaves at different heights.
@@ -171,7 +230,7 @@ func runBlockChallengeTest(t testing.TB, cfg *blockChallengeTestConfig) {
 		validators[i] = v
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
 	harnessObserver := make(chan protocol.ChallengeEvent, 1)
@@ -190,182 +249,32 @@ func runBlockChallengeTest(t testing.TB, cfg *blockChallengeTestConfig) {
 		time.Sleep(time.Millisecond * 100)
 	}
 	expectedEventIndex := 0
-	for {
-		if expectedEventIndex == len(cfg.eventsToAssert) {
-			t.Log("Finished asserting events")
-			for i := 0; i < int(cfg.numValidators); i++ {
-				AssertLogsContain(t, hook, "Reached a one-step-fork")
-			}
-			break
-		}
-		select {
-		case ev := <-harnessObserver:
-			t.Logf("%+T", ev)
-			wantedEv := cfg.eventsToAssert[expectedEventIndex]
-			wanted := reflect.TypeOf(wantedEv).Elem()
-			got := reflect.TypeOf(ev).Elem()
-			t.Logf("Asserting wanted event %+T against received %+T", wantedEv, ev)
-			require.Equal(t, wanted, got)
-
-			// Check the validator address is the one we expect from the sequence of events.
-			require.Equal(
-				t,
-				wantedEv.ValidatorAddress(),
-				ev.ValidatorAddress(),
-				fmt.Sprintf("event at index %d did not match", expectedEventIndex),
-			)
-			expectedEventIndex++
-		case <-ctx.Done():
+	for ev := range harnessObserver {
+		if ctx.Err() != nil {
 			t.Fatal("Timed out - validators were unable to reach one-step-fork in time")
 		}
-	}
-}
-
-func TestChallenges_ThreeValidatorsReachOneStepFork_Simple(t *testing.T) {
-	t.Skip()
-	// Similar to the two validator test, but with three validators.
-	//
-	// 1. alice, bob and charlies create their own leaves at height 6 they disagree with
-	// 2. alice bisects to 4, bob and charlie disagrees with the bisection and bisects to their own 4
-	// 3. alice bisects to 2, bob and charlie agrees and merges to 2
-	// 4. alice bisects from 4 to 3, bob and charlie agrees and merges to 3
-	// 5. they are now at a one-step fork, we now await subchallenge resolution.
-	hook := test.NewGlobal()
-	stateRootsInCommon := make([]common.Hash, 0)
-	for i := uint64(0); i < 3; i++ {
-		stateRootsInCommon = append(stateRootsInCommon, util.HashForUint(i))
-	}
-	aliceRoots := append(
-		stateRootsInCommon,
-		common.BytesToHash([]byte("a3")),
-		common.BytesToHash([]byte("a4")),
-		common.BytesToHash([]byte("a5")),
-		common.BytesToHash([]byte("a6")),
-	)
-	bobRoots := append(
-		stateRootsInCommon,
-		common.BytesToHash([]byte("b3")),
-		common.BytesToHash([]byte("b4")),
-		common.BytesToHash([]byte("b5")),
-		common.BytesToHash([]byte("b6")),
-	)
-	charlieRoots := append(
-		stateRootsInCommon,
-		common.BytesToHash([]byte("c3")),
-		common.BytesToHash([]byte("c4")),
-		common.BytesToHash([]byte("c5")),
-		common.BytesToHash([]byte("c6")),
-	)
-
-	aliceStateManager := statemanager.New(aliceRoots)
-	bobStateManager := statemanager.New(bobRoots)
-	charlieStateManager := statemanager.New(charlieRoots)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	chain := protocol.NewAssertionChain(ctx, util.NewArtificialTimeReference(), time.Second)
-	aliceAddr := common.BytesToAddress([]byte("a"))
-	bobAddr := common.BytesToAddress([]byte("b"))
-	charlieAddr := common.BytesToAddress([]byte("c"))
-
-	bal := big.NewInt(0).Mul(protocol.Gwei, big.NewInt(100))
-	err := chain.Tx(func(tx *protocol.ActiveTx, p protocol.OnChainProtocol) error {
-		chain.AddToBalance(tx, aliceAddr, bal)
-		chain.AddToBalance(tx, bobAddr, bal)
-		chain.AddToBalance(tx, charlieAddr, bal)
-		return nil
-	})
-	require.NoError(t, err)
-
-	alice, err := New(
-		ctx,
-		chain,
-		aliceStateManager,
-		WithName("alice"),
-		WithAddress(aliceAddr),
-		WithDisableLeafCreation(),
-	)
-	require.NoError(t, err)
-	bob, err := New(
-		ctx,
-		chain,
-		bobStateManager,
-		WithName("bob"),
-		WithAddress(bobAddr),
-		WithDisableLeafCreation(),
-	)
-	require.NoError(t, err)
-	charlie, err := New(
-		ctx,
-		chain,
-		charlieStateManager,
-		WithName("charlie"),
-		WithAddress(charlieAddr),
-		WithDisableLeafCreation(),
-	)
-	require.NoError(t, err)
-
-	harnessObserver := make(chan protocol.ChallengeEvent, 1)
-	chain.SubscribeChallengeEvents(ctx, harnessObserver)
-
-	_, err = alice.submitLeafCreation(ctx)
-	require.NoError(t, err)
-	_, err = bob.submitLeafCreation(ctx)
-	require.NoError(t, err)
-	_, err = charlie.submitLeafCreation(ctx)
-	require.NoError(t, err)
-
-	AssertLogsContain(t, hook, "Submitted leaf creation")
-	AssertLogsContain(t, hook, "Submitted leaf creation")
-	AssertLogsContain(t, hook, "Submitted leaf creation")
-
-	go alice.Start(ctx)
-	time.Sleep(time.Millisecond * 100)
-	go bob.Start(ctx)
-	time.Sleep(time.Millisecond * 100)
-	go charlie.Start(ctx)
-
-	eventsToAssert := []protocol.ChallengeEvent{
-		// TODO: Why is this passing?
-		&protocol.ChallengeLeafEvent{Validator: aliceAddr},
-		&protocol.ChallengeLeafEvent{Validator: aliceAddr},
-		&protocol.ChallengeLeafEvent{Validator: bobAddr},
-		&protocol.ChallengeLeafEvent{Validator: bobAddr},
-		&protocol.ChallengeBisectEvent{Validator: bobAddr},
-		&protocol.ChallengeBisectEvent{Validator: aliceAddr},
-		&protocol.ChallengeBisectEvent{Validator: aliceAddr},
-		&protocol.ChallengeMergeEvent{Validator: bobAddr},
-		&protocol.ChallengeBisectEvent{Validator: bobAddr},
-		&protocol.ChallengeMergeEvent{Validator: aliceAddr},
-	}
-	expectedEventIndex := 0
-	for {
-		if expectedEventIndex == len(eventsToAssert) {
-			t.Log("Finished asserting events")
-			AssertLogsContain(t, hook, "Reached a one-step-fork")
-			AssertLogsContain(t, hook, "Reached a one-step-fork")
-			AssertLogsContain(t, hook, "Reached a one-step-fork")
-			break
+		if expectedEventIndex >= len(cfg.eventsToAssert) {
+			t.Fatal("Received more events than were expected")
 		}
-		select {
-		case ev := <-harnessObserver:
-			t.Logf("%+T", ev)
-			wantedEv := eventsToAssert[expectedEventIndex]
-			wanted := reflect.TypeOf(wantedEv).Elem()
-			got := reflect.TypeOf(ev).Elem()
-			t.Logf("Asserting wanted event %+T against received %+T", wantedEv, ev)
-			require.Equal(t, wanted, got)
+		t.Logf("%+T", ev)
+		wantedEv := cfg.eventsToAssert[expectedEventIndex]
+		wanted := reflect.TypeOf(wantedEv).Elem()
+		got := reflect.TypeOf(ev).Elem()
+		t.Logf("Asserting wanted event %+T against received %+T", wantedEv, ev)
+		require.Equal(t, wanted, got)
 
-			// Check the validator address is the one we expect from the sequence of events.
-			require.Equal(
-				t,
-				wantedEv.ValidatorAddress(),
-				ev.ValidatorAddress(),
-				fmt.Sprintf("event at index %d did not match", expectedEventIndex),
-			)
-			expectedEventIndex++
-		case <-ctx.Done():
-			t.Fatal("Timed out - validators were unable to reach one-step-fork in time")
-		}
+		// Check the validator address is the one we expect from the sequence of events.
+		require.Equal(
+			t,
+			wantedEv.ValidatorAddress(),
+			ev.ValidatorAddress(),
+			fmt.Sprintf("event at index %d did not match", expectedEventIndex),
+		)
+		expectedEventIndex++
+	}
+	require.Equal(t, len(cfg.eventsToAssert), expectedEventIndex)
+	t.Log("Finished asserting events")
+	for i := 0; i < int(cfg.numValidators); i++ {
+		AssertLogsContain(t, hook, "Reached a one-step-fork")
 	}
 }
