b17c366880c6cba6ec1302f20b825e64d05d801c
rauljordan <raul@prysmaticlabs.com>
2023-01-30 23:53:53 -0500

    initial bindings

--- go.mod
+++ go.mod
@@ -13,11 +13,20 @@ require (
 )
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/go-ole/go-ole v1.2.1 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rjeczalik/notify v0.9.1 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.5 // indirect
+	github.com/tklauser/numcpus v0.2.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
---
+++ protocol/solidity/assertion_chain.go
@@ -0,0 +1,63 @@
+package solidity
+
+import (
+	"github.com/OffchainLabs/challenge-protocol-v2/protocol/bindings"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// TODO: Implement a nice wrapper around solgen bindings to the protocol contracts
+// that aligns with the AssertionManager interface. This aims to provide a clean API
+// that does not expose Ethereum internals to consumers.
+// TODO: Implement custom, nice wrapper types such as Assertion, ChallengeVertex, Challenge
+// which do not expose Ethereum internals to callers outside of this package.
+type AssertionChain struct {
+	backend bind.ContractBackend
+	chain   *bindings.AssertionChain
+}
+
+func NewAssertionChain() (*AssertionChain, error) {
+	chain, err := bindings.NewAssertionChain(common.Address{}, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &AssertionChain{
+		chain: chain,
+	}, nil
+}
+
+func (ac *AssertionChain) ChallengePeriodLength() (uint64, error) {
+	res, err := ac.chain.AssertionChainCaller.ChallengePeriodSeconds(&bind.CallOpts{})
+	if err != nil {
+		return 0, err
+	}
+	return res.Uint64(), nil
+}
+
+func (ac *AssertionChain) NumAssertions() (uint64, error) {
+	res, err := ac.chain.AssertionChainCaller.NumAssertions(&bind.CallOpts{})
+	if err != nil {
+		return 0, err
+	}
+	return res.Uint64(), nil
+}
+
+// // AssertionManager allows the creation of new leaves for a Staker with a State Commitment
+// // and a previous assertion.
+// type AssertionManager interface {
+// 	Inbox() *Inbox
+// 	NumAssertions(tx *ActiveTx) uint64
+// 	AssertionBySequenceNum(tx *ActiveTx, seqNum AssertionSequenceNumber) (*Assertion, error)
+// 	ChallengeByCommitHash(tx *ActiveTx, commitHash ChallengeCommitHash) (*Challenge, error)
+// 	ChallengeVertexByCommitHash(tx *ActiveTx, challenge ChallengeCommitHash, vertex VertexCommitHash) (*ChallengeVertex, error)
+// 	IsAtOneStepFork(
+// 		tx *ActiveTx,
+// 		challengeCommitHash ChallengeCommitHash,
+// 		vertexCommit util.HistoryCommitment,
+// 		vertexParentCommit util.HistoryCommitment,
+// 	) (bool, error)
+// 	ChallengePeriodLength(tx *ActiveTx) time.Duration
+// 	LatestConfirmed(*ActiveTx) *Assertion
+// 	CreateLeaf(tx *ActiveTx, prev *Assertion, commitment StateCommitment, staker common.Address) (*Assertion, error)
+// 	TimeReference() util.TimeReference
+// }
