fb9a672af96decb7a2108ec98d1ea691831f715d
Raul Jordan <raul@prysmaticlabs.com>
2023-06-20 16:21:54 -0400

    edge tracker package

--- challenge-manager/manager_test.go
+++ challenge-manager/manager_test.go
@@ -1,15 +1,123 @@
-package validator
+package challengemanager
 
 import (
 	"context"
+	"io"
 	"testing"
+	"time"
 
+	watcher "github.com/OffchainLabs/challenge-protocol-v2/challenge-manager/chain-watcher"
+	edgetracker "github.com/OffchainLabs/challenge-protocol-v2/challenge-manager/edge-tracker"
 	"github.com/OffchainLabs/challenge-protocol-v2/testing/mocks"
 	"github.com/OffchainLabs/challenge-protocol-v2/testing/setup"
+	customTime "github.com/OffchainLabs/challenge-protocol-v2/time"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 )
 
 var _ = ChallengeCreator(&Manager{})
+
+func init() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.SetOutput(io.Discard)
+}
+
+func TestEdgeTracker_act(t *testing.T) {
+	//ctx := context.Background()
+	t.Run("bisects", func(t *testing.T) {
+		// hook := test.NewGlobal()
+		// tkr, _ := setupNonPSTracker(ctx, t)
+		// err := tkr.act(ctx)
+		// require.NoError(t, err)
+		// require.Equal(t, int(0), int(tkr.fsm.Current().State))
+		// err = tkr.act(ctx)
+		// require.NoError(t, err)
+		// require.Equal(t, int(1), int(tkr.fsm.Current().State))
+		// logging.AssertLogsContain(t, hook, "Successfully bisected")
+		// err = tkr.act(ctx)
+		// require.NoError(t, err)
+		// require.Equal(t, int(2), int(tkr.fsm.Current().State))
+	})
+}
+
+func setupNonPSTracker(ctx context.Context, t *testing.T) (*edgetracker.Tracker, *edgetracker.Tracker) {
+	createdData, err := setup.CreateTwoValidatorFork(ctx, &setup.CreateForkConfig{})
+	require.NoError(t, err)
+
+	honestValidator, err := New(
+		ctx,
+		createdData.Chains[0],
+		createdData.Backend,
+		createdData.HonestStateManager,
+		createdData.Addrs.Rollup,
+		WithName("alice"),
+	)
+	require.NoError(t, err)
+
+	evilValidator, err := New(
+		ctx,
+		createdData.Chains[1],
+		createdData.Backend,
+		createdData.EvilStateManager,
+		createdData.Addrs.Rollup,
+		WithName("bob"),
+	)
+	require.NoError(t, err)
+
+	honestEdge, _, err := honestValidator.addBlockChallengeLevelZeroEdge(ctx, createdData.Leaf1)
+	require.NoError(t, err)
+
+	evilEdge, _, err := evilValidator.addBlockChallengeLevelZeroEdge(ctx, createdData.Leaf2)
+	require.NoError(t, err)
+
+	// Check presumptive statuses.
+	hasRival, err := honestEdge.HasRival(ctx)
+	require.NoError(t, err)
+	require.Equal(t, false, !hasRival)
+
+	honestWatcher := watcher.New(honestValidator.chain, honestValidator.stateManager, createdData.Backend, time.Second, "alice")
+	tracker1, err := edgetracker.New(
+		honestEdge,
+		honestValidator.chain,
+		createdData.HonestStateManager,
+		honestValidator.watcher,
+		honestValidator,
+		edgetracker.HeightConfig{
+			StartBlockHeight:           0,
+			TopLevelClaimEndBatchCount: 1,
+		},
+		edgetracker.WithTimeReference(customTime.NewArtificialTimeReference()),
+		edgetracker.WithValidatorAddress(honestValidator.address),
+		edgetracker.WithValidatorName(honestValidator.name),
+	)
+	require.NoError(t, err)
+
+	syncCompleted := make(chan struct{})
+	go honestWatcher.Watch(ctx, syncCompleted)
+	<-syncCompleted
+
+	evilWatcher := watcher.New(evilValidator.chain, evilValidator.stateManager, createdData.Backend, time.Second, "alice")
+	tracker2, err := edgetracker.New(
+		evilEdge,
+		evilValidator.chain,
+		createdData.EvilStateManager,
+		evilValidator.watcher,
+		evilValidator,
+		edgetracker.HeightConfig{
+			StartBlockHeight:           0,
+			TopLevelClaimEndBatchCount: 1,
+		},
+		edgetracker.WithTimeReference(customTime.NewArtificialTimeReference()),
+		edgetracker.WithValidatorAddress(evilValidator.address),
+		edgetracker.WithValidatorName(evilValidator.name),
+	)
+	require.NoError(t, err)
+
+	syncCompleted = make(chan struct{})
+	go evilWatcher.Watch(ctx, syncCompleted)
+	<-syncCompleted
+	return tracker1, tracker2
+}
 
 func setupValidator(t *testing.T) (*Manager, *mocks.MockProtocol, *mocks.MockStateManager) {
 	t.Helper()
