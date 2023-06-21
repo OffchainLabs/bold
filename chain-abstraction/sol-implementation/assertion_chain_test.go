3cfa68cc102dc29b1b894a9130de61d8e542cbde
Raul Jordan <raul@prysmaticlabs.com>
2023-06-21 11:41:06 -0400

    unrivaled

--- chain-abstraction/sol-implementation/assertion_chain_test.go
+++ chain-abstraction/sol-implementation/assertion_chain_test.go
@@ -77,7 +77,6 @@ func TestCreateAssertion(t *testing.T) {
 }
 
 func TestAssertionUnrivaledBlocks(t *testing.T) {
-	t.Skip()
 	ctx := context.Background()
 	cfg, err := setup.ChainsWithEdgeChallengeManager()
 	require.NoError(t, err)
