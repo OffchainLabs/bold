79d4d09198b60654b276561524d60725411045de
Raul Jordan <raul@prysmaticlabs.com>
2023-06-21 20:51:05 -0400

    fix

--- testing/endtoend/expect.go
+++ testing/endtoend/expect.go
@@ -20,15 +20,8 @@ type expect func(t *testing.T, ctx context.Context, be backend.Backend) error
 
 // Expects that an assertion is confirmed by challenge win.
 func expectAssertionConfirmedByChallengeWinner(t *testing.T, ctx context.Context, be backend.Backend) error {
-	t.Run("honest assertion confirmed by challenge win", func(t *testing.T) {
-		rc, err := retry.UntilSucceeds(ctx, func() (*rollupgen.RollupCore, error) {
-			return rollupgen.NewRollupCore(be.ContractAddresses().Rollup, be.Client())
-		})
-		require.NoError(t, err)
-
-		latestHonestStaked, err := retry.UntilSucceeds(ctx, func() ([32]byte, error) {
-			return rc.LatestStakedAssertion(&bind.CallOpts{Context: ctx}, be.Alice().From)
-		})
+	t.Run("assertion confirmed", func(t *testing.T) {
+		rc, err := rollupgen.NewRollupCore(be.ContractAddresses().Rollup, be.Client())
 		require.NoError(t, err)
 
 		var confirmed bool
@@ -36,22 +29,20 @@ func expectAssertionConfirmedByChallengeWinner(t *testing.T, ctx context.Context
 			i, err := retry.UntilSucceeds(ctx, func() (*rollupgen.RollupCoreAssertionConfirmedIterator, error) {
 				return rc.FilterAssertionConfirmed(nil, nil)
 			})
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			for i.Next() {
-				// Only check if the assertion id belongs to the honest staker.
-				if i.Event.AssertionId != latestHonestStaked {
-					continue
-				}
 				assertionNode, err := retry.UntilSucceeds(ctx, func() (rollupgen.AssertionNode, error) {
 					return rc.GetAssertion(&bind.CallOpts{Context: ctx}, i.Event.AssertionId)
 				})
-				require.NoError(t, err)
+				if err != nil {
+					t.Fatal(err)
+				}
 				if assertionNode.Status != uint8(protocol.AssertionConfirmed) {
 					t.Fatal("Confirmed assertion with unfinished state")
 				}
 				confirmed = true
-
-				require.NoError(t, err)
 				break
 			}
 			time.Sleep(500 * time.Millisecond) // Don't spam the backend.
@@ -71,19 +62,25 @@ func expectAliceAndBobStaked(t *testing.T, ctx context.Context, be backend.Backe
 		ecm, err := retry.UntilSucceeds(ctx, func() (*challengeV2gen.EdgeChallengeManager, error) {
 			return edgeManager(be)
 		})
-		require.NoError(t, err)
+		if err != nil {
+			t.Fatal(err)
+		}
 
 		var aliceStaked, bobStaked bool
 		for ctx.Err() == nil && (!aliceStaked || !bobStaked) {
 			i, err := retry.UntilSucceeds(ctx, func() (*challengeV2gen.EdgeChallengeManagerEdgeAddedIterator, error) {
 				return ecm.FilterEdgeAdded(nil, nil, nil, nil)
 			})
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			for i.Next() {
 				e, err := retry.UntilSucceeds(ctx, func() (challengeV2gen.ChallengeEdge, error) {
 					return ecm.GetEdge(nil, i.Event.EdgeId)
 				})
-				require.NoError(t, err)
+				if err != nil {
+					t.Fatal(err)
+				}
 
 				switch e.Staker {
 				case be.Alice().From:
