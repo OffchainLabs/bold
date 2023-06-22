1cbb79b68b172e6efaf28a0ccc2fe9bdf0ad1135
Raul Jordan <raul@prysmaticlabs.com>
2023-06-21 20:24:07 -0400

    simpler expect

--- testing/endtoend/basic_local_test.go
+++ testing/endtoend/basic_local_test.go
@@ -10,7 +10,6 @@ import (
 	solimpl "github.com/OffchainLabs/challenge-protocol-v2/chain-abstraction/sol-implementation"
 	validator "github.com/OffchainLabs/challenge-protocol-v2/challenge-manager"
 	l2stateprovider "github.com/OffchainLabs/challenge-protocol-v2/layer2-state-provider"
-	retry "github.com/OffchainLabs/challenge-protocol-v2/runtime"
 	challenge_testing "github.com/OffchainLabs/challenge-protocol-v2/testing"
 	"github.com/OffchainLabs/challenge-protocol-v2/testing/endtoend/internal/backend"
 	"github.com/OffchainLabs/challenge-protocol-v2/testing/toys/assertions"
@@ -49,9 +48,13 @@ type challengeProtocolTestConfig struct {
 
 func TestChallengeProtocol_AliceAndBob_AnvilLocal_SameHeight(t *testing.T) {
 	be, err := backend.NewAnvilLocal(context.Background())
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	require.NoError(t, be.Start())
+	if err := be.Start(); err != nil {
+		t.Fatal(err)
+	}
 	defer func() {
 		if err := be.Stop(); err != nil {
 			t.Log(fmt.Errorf("error stopping backend: %v", err))
@@ -62,7 +65,9 @@ func TestChallengeProtocol_AliceAndBob_AnvilLocal_SameHeight(t *testing.T) {
 		Name: "two forked assertions at the same height",
 		AliceStateManager: func() l2stateprovider.Provider {
 			sm, err := statemanager.NewForSimpleMachine()
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			return sm
 		}(),
 		BobStateManager: func() l2stateprovider.Provider {
@@ -95,18 +100,26 @@ func TestChallengeProtocol_AliceAndBob_AnvilLocal_SameHeight(t *testing.T) {
 func TestChallengeProtocol_AliceAndBob_AnvilLocal_DifferentHeights(t *testing.T) {
 	t.Skip()
 	be, err := backend.NewAnvilLocal(context.Background())
-	require.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	require.NoError(t, be.Start())
+	if err := be.Start(); err != nil {
+		t.Fatal(err)
+	}
 	defer func() {
-		require.NoError(t, be.Stop(), "error stopping backend")
+		if err := be.Stop(); err != nil {
+			t.Log(fmt.Errorf("error stopping backend: %v", err))
+		}
 	}()
 
 	scenario := &ChallengeScenario{
 		Name: "two forked assertions at the different step heights",
 		AliceStateManager: func() l2stateprovider.Provider {
 			sm, err := statemanager.NewForSimpleMachine()
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			return sm
 		}(),
 		BobStateManager: func() l2stateprovider.Provider {
@@ -122,7 +135,9 @@ func TestChallengeProtocol_AliceAndBob_AnvilLocal_DifferentHeights(t *testing.T)
 				statemanager.WithBlockDivergenceHeight(cfg.assertionDivergenceHeight),
 				statemanager.WithDivergentBlockHeightOffset(cfg.assertionBlockHeightDifference),
 			)
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			return sm
 		}(),
 		Expectations: []expect{
@@ -156,17 +171,23 @@ func TestSync_HonestBobStopsCharlieJoins(t *testing.T) {
 				statemanager.WithBlockDivergenceHeight(cfg.assertionDivergenceHeight),
 				statemanager.WithDivergentBlockHeightOffset(cfg.assertionBlockHeightDifference),
 			)
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			return sm
 		}(),
 		BobStateManager: func() l2stateprovider.Provider {
 			sm, err := statemanager.NewForSimpleMachine()
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			return sm
 		}(),
 		CharlieStateManager: func() l2stateprovider.Provider {
 			sm, err := statemanager.NewForSimpleMachine()
-			require.NoError(t, err)
+			if err != nil {
+				t.Fatal(err)
+			}
 			return sm
 		}(),
 		Expectations: []expect{
@@ -182,43 +203,43 @@ func testChallengeProtocol_AliceAndBob(t *testing.T, be backend.Backend, scenari
 		ctx, cancel := context.WithTimeout(context.Background(), time.Minute*10)
 		defer cancel()
 
-		rollup, err := retry.UntilSucceeds(ctx, func() (common.Address, error) {
-			return be.DeployRollup()
-		})
-		require.NoError(t, err)
+		rollup, err := be.DeployRollup()
+		if err != nil {
+			t.Fatal(err)
+		}
 
 		a, aChain, err := setupValidator(ctx, be, rollup, scenario.AliceStateManager, be.Alice(), "alice")
-		require.NoError(t, err)
+		if err != nil {
+			t.Fatal(err)
+		}
 		b, bChain, err := setupValidator(ctx, be, rollup, scenario.BobStateManager, be.Bob(), "bob")
-		require.NoError(t, err)
+		if err != nil {
+			t.Fatal(err)
+		}
 
 		// Post assertions.
 		alicePoster := assertions.NewPoster(aChain, scenario.AliceStateManager, "alice", time.Hour)
 		bobPoster := assertions.NewPoster(bChain, scenario.BobStateManager, "bob", time.Hour)
 
-		aliceLeaf, err := retry.UntilSucceeds(ctx, func() (protocol.Assertion, error) {
-			return alicePoster.PostLatestAssertion(ctx)
-		})
-		require.NoError(t, err)
-		bobLeaf, err := retry.UntilSucceeds(ctx, func() (protocol.Assertion, error) {
-			return bobPoster.PostLatestAssertion(ctx)
-		})
-		require.NoError(t, err)
+		aliceLeaf, err := alicePoster.PostLatestAssertion(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+		bobLeaf, err := bobPoster.PostLatestAssertion(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
 
 		// Scan for created assertions.
 		aliceScanner := assertions.NewScanner(aChain, scenario.AliceStateManager, be.Client(), a, rollup, "alice", time.Hour)
 		bobScanner := assertions.NewScanner(bChain, scenario.BobStateManager, be.Client(), b, rollup, "bob", time.Hour)
 
-		_, err = retry.UntilSucceeds(ctx, func() (bool, error) {
-			processErr := aliceScanner.ProcessAssertionCreation(ctx, aliceLeaf.Id())
-			return false, processErr
-		})
-		require.NoError(t, err)
-		_, err = retry.UntilSucceeds(ctx, func() (bool, error) {
-			processErr := bobScanner.ProcessAssertionCreation(ctx, bobLeaf.Id())
-			return false, processErr
-		})
-		require.NoError(t, err)
+		if err := aliceScanner.ProcessAssertionCreation(ctx, aliceLeaf.Id()); err != nil {
+			panic(err)
+		}
+		if err := bobScanner.ProcessAssertionCreation(ctx, bobLeaf.Id()); err != nil {
+			panic(err)
+		}
 
 		a.Start(ctx)
 		b.Start(ctx)
@@ -231,7 +252,9 @@ func testChallengeProtocol_AliceAndBob(t *testing.T, be backend.Backend, scenari
 			})
 		}
 
-		require.NoError(t, g.Wait())
+		if err := g.Wait(); err != nil {
+			t.Fatal(err)
+		}
 	})
 }
 
@@ -240,45 +263,37 @@ func testSyncBobStopsCharlieJoins(t *testing.T, be backend.Backend, s *Challenge
 	t.Run(s.Name, func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Minute*10)
 		defer cancel()
-		rollup, err := retry.UntilSucceeds(ctx, func() (common.Address, error) {
-			return be.DeployRollup()
-		})
+
+		rollup, err := be.DeployRollup()
 		require.NoError(t, err)
 
+		hr := headerreader.New(be.Client(), func() *headerreader.Config {
+			return &headerreader.DefaultConfig
+		})
+
 		// Bad Alice
-		alice, aChain, err := setupValidator(ctx, be, rollup, s.AliceStateManager, be.Alice(), "alice")
+		aChain, err := solimpl.NewAssertionChain(ctx, rollup, be.Alice(), be.Client(), hr)
+		require.NoError(t, err)
+		alice, err := validator.New(ctx, aChain, be.Client(), s.AliceStateManager, rollup, validator.WithAddress(be.Alice().From), validator.WithName("alice"))
 		require.NoError(t, err)
 
 		// Good Bob
 		bobCtx, bobCancelCtx := context.WithCancel(ctx)
-		bob, bChain, err := setupValidator(bobCtx, be, rollup, s.BobStateManager, be.Bob(), "bob")
+		bChain, err := solimpl.NewAssertionChain(bobCtx, rollup, be.Bob(), be.Client(), hr)
+		require.NoError(t, err)
+		bob, err := validator.New(bobCtx, bChain, be.Client(), s.BobStateManager, rollup, validator.WithAddress(be.Bob().From), validator.WithName("bob"))
 		require.NoError(t, err)
 
 		alicePoster := assertions.NewPoster(aChain, s.AliceStateManager, "alice", time.Hour)
 		bobPoster := assertions.NewPoster(bChain, s.BobStateManager, "bob", time.Hour)
-
-		aliceLeaf, err := retry.UntilSucceeds(ctx, func() (protocol.Assertion, error) {
-			return alicePoster.PostLatestAssertion(ctx)
-		})
+		aliceLeaf, err := alicePoster.PostLatestAssertion(ctx)
 		require.NoError(t, err)
-		bobLeaf, err := retry.UntilSucceeds(ctx, func() (protocol.Assertion, error) {
-			return bobPoster.PostLatestAssertion(ctx)
-		})
+		bobLeaf, err := bobPoster.PostLatestAssertion(bobCtx)
 		require.NoError(t, err)
-
 		aliceScanner := assertions.NewScanner(aChain, s.AliceStateManager, be.Client(), alice, rollup, "alice", time.Hour)
 		bobScanner := assertions.NewScanner(bChain, s.BobStateManager, be.Client(), bob, rollup, "bob", time.Hour)
-
-		_, err = retry.UntilSucceeds(ctx, func() (bool, error) {
-			processErr := aliceScanner.ProcessAssertionCreation(ctx, aliceLeaf.Id())
-			return false, processErr
-		})
-		require.NoError(t, err)
-		_, err = retry.UntilSucceeds(ctx, func() (bool, error) {
-			processErr := bobScanner.ProcessAssertionCreation(ctx, bobLeaf.Id())
-			return false, processErr
-		})
-		require.NoError(t, err)
+		require.NoError(t, aliceScanner.ProcessAssertionCreation(ctx, aliceLeaf.Id()))
+		require.NoError(t, bobScanner.ProcessAssertionCreation(bobCtx, bobLeaf.Id()))
 
 		// Alice and bob starts to challenge each other.
 		alice.Start(ctx)
@@ -289,9 +304,10 @@ func testSyncBobStopsCharlieJoins(t *testing.T, be backend.Backend, s *Challenge
 		bobCancelCtx()
 
 		// Good Charlie joins
-		charlie, _, err := setupValidator(ctx, be, rollup, s.CharlieStateManager, be.Charlie(), "charlie")
+		cChain, err := solimpl.NewAssertionChain(ctx, rollup, be.Charlie(), be.Client(), hr)
 		require.NoError(t, err)
-
+		charlie, err := validator.New(ctx, cChain, be.Client(), s.CharlieStateManager, rollup, validator.WithAddress(be.Charlie().From), validator.WithName("charlie"))
+		require.NoError(t, err)
 		charlie.Start(ctx)
 
 		g, ctx := errgroup.WithContext(ctx)
@@ -322,31 +338,27 @@ func setupValidator(
 		return &headerreader.DefaultConfig
 	})
 
-	chain, err := retry.UntilSucceeds(ctx, func() (*solimpl.AssertionChain, error) {
-		return solimpl.NewAssertionChain(
-			ctx,
-			rollup,
-			txOpts,
-			be.Client(),
-			hr,
-		)
-	})
+	chain, err := solimpl.NewAssertionChain(
+		ctx,
+		rollup,
+		txOpts,
+		be.Client(),
+		hr,
+	)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	v, err := retry.UntilSucceeds(ctx, func() (*validator.Manager, error) {
-		return validator.New(
-			ctx,
-			chain,
-			be.Client(),
-			sm,
-			rollup,
-			validator.WithAddress(txOpts.From),
-			validator.WithName(name),
-			validator.WithEdgeTrackerWakeInterval(time.Millisecond*250),
-		)
-	})
+	v, err := validator.New(
+		ctx,
+		chain,
+		be.Client(),
+		sm,
+		rollup,
+		validator.WithAddress(txOpts.From),
+		validator.WithName(name),
+		validator.WithEdgeTrackerWakeInterval(time.Millisecond*250),
+	)
 	if err != nil {
 		return nil, nil, err
 	}
