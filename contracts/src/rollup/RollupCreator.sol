46c909c35b2f126190d3dadfb06510da973c95c8
gzeon <im@gzeon.dev>
2023-08-02 02:59:45 +0100

    chore: nitro-contracts/pull/37 and 39

--- contracts/src/rollup/RollupCreator.sol
+++ contracts/src/rollup/RollupCreator.sol
@@ -11,6 +11,7 @@ import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.so
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./RollupProxy.sol";
+import "./IRollupAdmin.sol";
 
 contract RollupCreator is Ownable {
     event RollupCreated(
@@ -78,12 +79,28 @@ contract RollupCreator is Ownable {
         return challengeManager;
     }
 
-    // After this setup:
-    // Rollup should be the owner of bridge
-    // RollupOwner should be the owner of Rollup's ProxyAdmin
-    // RollupOwner should be the owner of Rollup
-    // Bridge should have a single inbox and outbox
-    function createRollup(Config calldata config) external returns (address) {
+    function createRollup(Config memory config) external returns (address) {
+        return createRollup(config, address(0), new address[](0));
+    }
+
+    /**
+     * @notice Create a new rollup
+     * @dev After this setup:
+     * @dev - Rollup should be the owner of bridge
+     * @dev - RollupOwner should be the owner of Rollup's ProxyAdmin
+     * @dev - RollupOwner should be the owner of Rollup
+     * @dev - Bridge should have a single inbox and outbox
+     * @dev - Validators and batch poster should be set if provided
+     * @param config       The configuration for the rollup
+     * @param _batchPoster The address of the batch poster, not used when set to zero address
+     * @param _validators  The list of validator addresses, not used when set to empty list
+     * @return The address of the newly created rollup
+     */
+    function createRollup(
+        Config memory config,
+        address _batchPoster,
+        address[] memory _validators
+    ) public returns (address) {        
         ProxyAdmin proxyAdmin = new ProxyAdmin();
         proxyAdmin.transferOwnership(config.owner);
 
@@ -100,6 +117,11 @@ contract RollupCreator is Ownable {
 
         IEdgeChallengeManager challengeManager = createChallengeManager(address(rollup), address(proxyAdmin), config);
 
+        // initialize the rollup with this contract as owner to set batch poster and validators
+        // it will transfer the ownership back to the actual owner later
+        address actualOwner = config.owner;
+        config.owner = address(this);
+
         rollup.initializeProxy(
             config,
             ContractDependencies({
@@ -115,6 +137,20 @@ contract RollupCreator is Ownable {
                 validatorWalletCreator: validatorWalletCreator
             })
         );
+
+        // setting batch poster, if the address provided is not zero address
+        if (_batchPoster != address(0)) {
+            sequencerInbox.setIsBatchPoster(_batchPoster, true);
+        }
+        // Call setValidator on the newly created rollup contract just if validator set is not empty
+        if (_validators.length != 0) {
+            bool[] memory _vals = new bool[](_validators.length);
+            for (uint256 i = 0; i < _validators.length; i++) {
+                _vals[i] = true;
+            }
+            IRollupAdmin(address(rollup)).setValidator(_validators, _vals);
+        }
+        IRollupAdmin(address(rollup)).setOwner(actualOwner);
 
         emit RollupCreated(
             address(rollup), address(inbox), address(proxyAdmin), address(sequencerInbox), address(bridge)
