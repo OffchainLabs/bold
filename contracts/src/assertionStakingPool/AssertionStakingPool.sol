7da91a16398d638405ffac3e2232634edd23b99b
dzgoldman <dzgoldman@wesleyan.edu>
2023-08-09 17:21:19 -0400

    AssertionStakingPool first pass

---
+++ contracts/src/assertionStakingPool/AssertionStakingPool.sol
@@ -0,0 +1,97 @@
+// Copyright 2021-2022, Offchain Labs, Inc.
+// For license information, see https://github.com/nitro/blob/master/LICENSE
+// SPDX-License-Identifier: BUSL-1.1
+//
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../rollup/IRollupLogic.sol";
+import "../rollup/IRollupCore.sol";
+import "./StakingPoolErrors.sol";
+
+contract AssertionStakingPoolCreator {
+    function createPoolForAssertion(address _rollup, bytes32 _expectedAssertionHash) external {
+        new AssertionStakingPool(_rollup, _expectedAssertionHash);
+    }
+}
+
+contract AssertionStakingPool {
+    using SafeERC20 for IERC20;
+
+    address public immutable rollup;
+    bytes32 public immutable assertionHash;
+    IERC20 public immutable stakeToken;
+    uint256 public immutable baseStake;
+
+    mapping(address => uint256) public depoisttedTokenBalances;
+
+    PoolState public poolState = PoolState.PENDING;
+
+    constructor(address _rollup, bytes32 _assertionHash) {
+        rollup = _rollup;
+        assertionHash = _assertionHash;
+        stakeToken = IERC20(IRollupCore(rollup).stakeToken());
+        baseStake = IRollupCore(rollup).baseStake();
+
+        stakeToken.approve(rollup, baseStake);
+    }
+
+    function depositIntoPool(uint256 _amount) external {
+        if (poolState != PoolState.PENDING) {
+            revert PoolNotInPendingState(poolState);
+        }
+
+        uint256 currentPoolBalance = stakeToken.balanceOf(address(this));
+        if (currentPoolBalance >= baseStake) {
+            revert PoolStakeAlreadyReached(baseStake);
+        }
+
+        uint256 amountToTransfer;
+
+        if (currentPoolBalance + _amount > baseStake) {
+            amountToTransfer = baseStake - currentPoolBalance;
+        } else {
+            amountToTransfer = _amount;
+        }
+
+        stakeToken.safeTransferFrom(msg.sender, address(this), amountToTransfer);
+        depoisttedTokenBalances[msg.sender] += amountToTransfer;
+    }
+
+    function createAssertion(AssertionInputs calldata assertionInputs) external {
+        if (poolState != PoolState.PENDING) {
+            revert PoolNotInPendingState(poolState);
+        }
+
+        uint256 balance = stakeToken.balanceOf(address(this));
+
+        if (balance < baseStake) {
+            revert NotEnoughStake(balance, baseStake);
+        }
+
+        IRollupUser(rollup).newStakeOnNewAssertion(baseStake, assertionInputs, assertionHash);
+        poolState = PoolState.ASSERTED;
+    }
+
+    function returnOldStakeBackToPool() external {
+        if (poolState != PoolState.ASSERTED) {
+            revert PoolNotInAssertedState(poolState);
+        }
+
+        IRollupUser(rollup).returnOldDeposit();
+        poolState = PoolState.CONFIRMED;
+    }
+
+    function withdrawFromPool() external {
+        if (poolState == PoolState.ASSERTED) {
+            revert PoolNotInPendingOrConfirmedState(poolState);
+        }
+        if (depoisttedTokenBalances[msg.sender] == 0) {
+            revert NoBalanceToWithdraw(msg.sender);
+        }
+
+        stakeToken.safeTransfer(msg.sender, depoisttedTokenBalances[msg.sender]);
+        depoisttedTokenBalances[msg.sender] = 0;
+    }
+}
---
+++ contracts/src/assertionStakingPool/StakingPoolErrors.sol
@@ -0,0 +1,23 @@
+// Copyright 2021-2022, Offchain Labs, Inc.
+// For license information, see https://github.com/nitro/blob/master/LICENSE
+// SPDX-License-Identifier: BUSL-1.1
+//
+pragma solidity ^0.8.0;
+
+enum PoolState {
+    PENDING,
+    ASSERTED,
+    CONFIRMED
+}
+
+error PoolNotInPendingState(PoolState poolState);
+
+error PoolNotInAssertedState(PoolState poolState);
+
+error PoolNotInPendingOrConfirmedState(PoolState poolState);
+
+error PoolStakeAlreadyReached(uint256 baseStake);
+
+error NotEnoughStake(uint256 balance, uint256 baseStake);
+
+error NoBalanceToWithdraw(uint256 sender);
