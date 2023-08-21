f2f7d18ee390377567645117af2913471a3d1bdc
Chris Buckland <cpbuckland88@gmail.com>
2023-08-21 16:03:02 +0200

    Added back edge type enum

--- contracts/src/challengeV2/libraries/Enums.sol
+++ contracts/src/challengeV2/libraries/Enums.sol
@@ -11,3 +11,16 @@ enum EdgeStatus {
     Pending,
     Confirmed
 }
+
+/// @notice The type of the edge. Challenges are decomposed into 3 types of subchallenge
+///         represented here by the edge type. Edges are initially created of type Block
+///         and are then bisected until they have length one. After that new BigStep edges are
+///         added that claim a Block type edge, and are then bisected until they have length one.
+///         Then a SmallStep edge is added that claims a length one BigStep edge, and these
+///         SmallStep edges are bisected until they reach length one. A length one small step edge
+///         can then be directly executed using a one-step proof.
+enum EdgeType {
+    Block,
+    BigStep,
+    SmallStep
+}
