4110b7349d8a0fa77a5e669c06141af425961610
Raul Jordan <raul@prysmaticlabs.com>
2023-09-05 21:04:55 -0400

    include local timer instead of path timer in logic

--- challenge-manager/challenge-tree/generalized_path_timer.go
+++ challenge-manager/challenge-tree/generalized_path_timer.go
@@ -9,12 +9,16 @@ import (
 
 	protocol "github.com/OffchainLabs/bold/chain-abstraction"
 	"github.com/OffchainLabs/bold/containers"
+	bisection "github.com/OffchainLabs/bold/math"
 	"github.com/pkg/errors"
 )
 
+// EdgeLocalTimer is the local unrivaled timer of a specific edge (not a cumulative path timer).
+type EdgeLocalTimer uint64
+
 type ancestorsQueryResponse struct {
-	ancestorCumulativePathTimers []PathTimer
-	ancestorEdgeIds              HonestAncestors
+	ancestorLocalTimers []EdgeLocalTimer
+	ancestorEdgeIds     HonestAncestors
 }
 
 // computeAncestorsWithTimers computes the ancestors of the given edge and their respective path timers, even
@@ -39,6 +43,7 @@ func (ht *HonestChallengeTree) computeAncestorsWithTimers(
 	currentEdge := startEdge
 
 	ancestry := make([]protocol.EdgeId, 0)
+	localTimers := make([]EdgeLocalTimer, 0)
 
 	for currentChallengeLevel < protocol.ChallengeLevel(ht.totalChallengeLevels) {
 		// Compute the root edge for the current challenge level.
@@ -48,7 +53,7 @@ func (ht *HonestChallengeTree) computeAncestorsWithTimers(
 		}
 
 		// Compute the ancestors for the current edge in the current challenge level.
-		_, ancestorsAtLevel, err := ht.findAncestorsInChallenge(ctx, rootEdge, currentEdge, blockNumber)
+		ancestorLocalTimers, ancestorsAtLevel, err := ht.findHonestAncestorsWithinChallengeLevel(ctx, rootEdge, currentEdge, blockNumber)
 		if err != nil {
 			return nil, err
 		}
@@ -57,10 +62,11 @@ func (ht *HonestChallengeTree) computeAncestorsWithTimers(
 		currentChallengeLevel += 1
 
 		// Expand the total ancestry list. We want ancestors from
-		// the bottom-up, so we must reverse the output slice
-		// from the find function.
+		// the bottom-up, so we must reverse the output slice from the find function.
+		containers.Reverse(ancestorLocalTimers)
 		containers.Reverse(ancestorsAtLevel)
 		ancestry = append(ancestry, ancestorsAtLevel...)
+		localTimers = append(localTimers, ancestorLocalTimers...)
 
 		if currentChallengeLevel == protocol.ChallengeLevel(ht.totalChallengeLevels) {
 			break
@@ -72,17 +78,22 @@ func (ht *HonestChallengeTree) computeAncestorsWithTimers(
 		if err != nil {
 			return nil, err
 		}
+		claimEdgeLocalTimer, err := ht.localTimer(nextLevelClaimedEdge, blockNumber)
+		if err != nil {
+			return nil, err
+		}
 		currentEdge = nextLevelClaimedEdge
 
 		// Include the next level claimed edge in the ancestry list.
 		ancestry = append(ancestry, nextLevelClaimedEdge.Id())
+		localTimers = append(localTimers, EdgeLocalTimer(claimEdgeLocalTimer))
 	}
 
 	// If the ancestry is empty, we just return an empty response.
 	if len(ancestry) == 0 {
 		return &ancestorsQueryResponse{
-			ancestorCumulativePathTimers: make([]PathTimer, 0),
-			ancestorEdgeIds:              ancestry,
+			ancestorLocalTimers: make([]EdgeLocalTimer, 0),
+			ancestorEdgeIds:     ancestry,
 		}, nil
 	}
 
@@ -103,25 +114,75 @@ func (ht *HonestChallengeTree) computeAncestorsWithTimers(
 			rootChallengeEdgeId,
 		)
 	}
-
-	// In addition, each path timer returned should also take into account
-	// the assertion unrivaled timer from the assertion chain. To do this, we compute it here and add it to
-	// each entry path timer before we return it.
-	assertionUnrivaledTimer, err := ht.metadataReader.AssertionUnrivaledBlocks(
-		ctx, ht.topLevelAssertionHash,
-	)
-	if err != nil {
-		return nil, err
-	}
-	ancestorCumulativeTimers := make([]PathTimer, 0)
-	for i := range ancestorCumulativeTimers {
-		ancestorCumulativeTimers[i] += PathTimer(assertionUnrivaledTimer)
-	}
-
 	return &ancestorsQueryResponse{
-		ancestorCumulativePathTimers: ancestorCumulativeTimers,
-		ancestorEdgeIds:              ancestry,
+		ancestorLocalTimers: localTimers,
+		ancestorEdgeIds:     ancestry,
 	}, nil
+}
+
+// Computes the list of ancestors in a challenge type from a root edge down
+// to a specified child edge within a single challenge level. The edge we are querying must be
+// a child of this start edge for this function to succeed without error.
+func (ht *HonestChallengeTree) findHonestAncestorsWithinChallengeLevel(
+	ctx context.Context,
+	start protocol.ReadOnlyEdge,
+	queryingFor protocol.ReadOnlyEdge,
+	blockNumber uint64,
+) ([]EdgeLocalTimer, []protocol.EdgeId, error) {
+	found := false
+	curr := start
+	ancestry := make([]protocol.EdgeId, 0)
+	localTimers := make([]EdgeLocalTimer, 0)
+	wantedEdgeStart, _ := queryingFor.StartCommitment()
+
+	for {
+		if ctx.Err() != nil {
+			return nil, nil, ctx.Err()
+		}
+		if curr.Id() == queryingFor.Id() {
+			found = true
+			break
+		}
+		ancestry = append(ancestry, curr.Id())
+
+		currStart, _ := curr.StartCommitment()
+		currEnd, _ := curr.EndCommitment()
+		bisectTo, err := bisection.Bisect(uint64(currStart), uint64(currEnd))
+		if err != nil {
+			return nil, nil, errors.Wrapf(err, "could not bisect start=%d, end=%d", currStart, currEnd)
+		}
+		// If the wanted edge's start commitment is < the bisection height of the current
+		// edge in the loop, it means it is part of its lower children.
+		if uint64(wantedEdgeStart) < bisectTo {
+			lowerSnapshot, lowerErr := curr.LowerChild(ctx)
+			if lowerErr != nil {
+				return nil, nil, errors.Wrapf(lowerErr, "could not get lower child for edge %#x", curr.Id())
+			}
+			if lowerSnapshot.IsNone() {
+				return nil, nil, fmt.Errorf("edge %#x had no lower child", curr.Id())
+			}
+			curr = ht.edges.Get(lowerSnapshot.Unwrap())
+		} else {
+			// Else, it is part of the upper children.
+			upperSnapshot, upperErr := curr.UpperChild(ctx)
+			if upperErr != nil {
+				return nil, nil, errors.Wrapf(upperErr, "could not get upper child for edge %#x", curr.Id())
+			}
+			if upperSnapshot.IsNone() {
+				return nil, nil, fmt.Errorf("edge %#x had no upper child", curr.Id())
+			}
+			curr = ht.edges.Get(upperSnapshot.Unwrap())
+		}
+		timer, err := ht.localTimer(curr, blockNumber)
+		if err != nil {
+			return nil, nil, err
+		}
+		localTimers = append(localTimers, EdgeLocalTimer(timer))
+	}
+	if !found {
+		return nil, nil, errNotFound(queryingFor.Id())
+	}
+	return localTimers, ancestry, nil
 }
 
 // Computes the root edge for a given child edge at a challenge level.
