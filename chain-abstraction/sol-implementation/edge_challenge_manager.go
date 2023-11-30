08763e209f1702b7eff55ced8c8429484c810dbf
Raul Jordan <raul@prysmaticlabs.com>
2023-11-30 14:14:39 -0600

    fix up go side

--- chain-abstraction/sol-implementation/edge_challenge_manager.go
+++ chain-abstraction/sol-implementation/edge_challenge_manager.go
@@ -811,6 +811,9 @@ func (cm *specChallengeManager) AddBlockChallengeLevelZeroEdge(
 	if err == nil && !someLevelZeroEdge.IsNone() {
 		return &honestEdge{someLevelZeroEdge.Unwrap()}, nil
 	}
+	maxUint256 := new(big.Int)
+	maxUint256.Exp(big.NewInt(2), big.NewInt(256), nil).Sub(maxUint256, big.NewInt(1))
+
 	args := challengeV2gen.CreateEdgeArgs{
 		Level:          protocol.NewBlockChallengeLevel().Uint8(),
 		EndHistoryRoot: endCommit.Merkle,
@@ -818,7 +821,7 @@ func (cm *specChallengeManager) AddBlockChallengeLevelZeroEdge(
 		ClaimId:        assertionCreation.AssertionHash,
 		PrefixProof:    startEndPrefixProof,
 		Proof:          blockEdgeProof,
-		MaxStakeAmount: nil, // TODO: Allow customization.
+		MaxStakeAmount: maxUint256, // TODO: Allow customization.
 	}
 	receipt, err := cm.assertionChain.transact(ctx, cm.backend, func(opts *bind.TransactOpts) (*types.Transaction, error) {
 		return cm.writer.CreateLayerZeroEdge(
@@ -922,6 +925,8 @@ func (cm *specChallengeManager) AddSubChallengeLevelZeroEdge(
 	if err != nil {
 		return nil, err
 	}
+	maxUint256 := new(big.Int)
+	maxUint256.Exp(big.NewInt(2), big.NewInt(256), nil).Sub(maxUint256, big.NewInt(1))
 	_, err = cm.assertionChain.transact(ctx, cm.backend, func(opts *bind.TransactOpts) (*types.Transaction, error) {
 		return cm.writer.CreateLayerZeroEdge(
 			opts,
@@ -932,6 +937,7 @@ func (cm *specChallengeManager) AddSubChallengeLevelZeroEdge(
 				ClaimId:        challengedEdge.Id().Hash,
 				PrefixProof:    startEndPrefixProof,
 				Proof:          subchallengeEdgeProof,
+				MaxStakeAmount: maxUint256, // TODO: Allow customization.
 			},
 		)
 	})
