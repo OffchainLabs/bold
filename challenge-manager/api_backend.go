074a6b6bbf36ad8a372a0e00c3af61654e998cb4
Raul Jordan <raul@prysmaticlabs.com>
2023-12-12 08:39:36 -0600

    begin chal api

--- challenge-manager/api_backend.go
+++ challenge-manager/api_backend.go
@@ -1,3 +1,12 @@
 package challengemanager
 
-func (m *Manager) checkHonestValidatorActivity() {}
+// Checks if there are active, honest validators involved onchain.
+func (m *Manager) checkHonestValidatorActivity() {
+	// - Identify the honest party by its history commitment in a challenge
+	// - Make sure it is the unrivaled and making txs. Check if its edges are being bisected or if subchallenges are being opened on its edges
+	// - Show the time between challenge moves
+	// - **If an honest edge is rivaled and is not being bisected for some X period of time, the honest validator might be down**
+	// How can we do this?
+
+	// First, identify the challenge
+}
