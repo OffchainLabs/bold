44c2df9225b56c1e4a9c36478a92326b72ec332d
Raul Jordan <raul@prysmaticlabs.com>
2024-01-26 17:07:50 -0600

    use the cache

--- containers/in-progress-cache/cache.go
+++ containers/in-progress-cache/cache.go
@@ -2,25 +2,25 @@ package inprogresscache
 
 import "sync"
 
-// InProgressCache implements a cache for expensive computations that ensures only
+// Cache for expensive computations that ensures only
 // one request is in-flight at a time. If a future request comes in with the same request id
 // as the ongoing computation, a goroutine is spawned that awaits the computation's completion
 // instead of kicking off two expensive computations.
-type InProgressCache[K comparable, V any] struct {
+type Cache[K comparable, V any] struct {
 	inProgress         map[K]bool
 	awaitingCompletion map[K][]chan V
 	lock               sync.RWMutex
 }
 
-func New[K comparable, V any]() *InProgressCache[K, V] {
-	return &InProgressCache[K, V]{
+func New[K comparable, V any]() *Cache[K, V] {
+	return &Cache[K, V]{
 		inProgress:         make(map[K]bool),
 		awaitingCompletion: make(map[K][]chan V),
 	}
 }
 
 // Compute an expensive closure. The request must be representable as a string.
-func (c *InProgressCache[K, V]) Compute(requestId K, f func() (V, error)) (V, error) {
+func (c *Cache[K, V]) Compute(requestId K, f func() (V, error)) (V, error) {
 	c.lock.RLock()
 	if ok := c.inProgress[requestId]; ok {
 		c.lock.RUnlock()
