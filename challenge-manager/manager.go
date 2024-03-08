b3de454f4fe2e547c351ba16373b4027dba6e33f
Raul Jordan <raul@prysmaticlabs.com>
2024-03-08 15:11:08 -0600

    fix

--- challenge-manager/manager.go
+++ challenge-manager/manager.go
@@ -179,7 +179,6 @@ func New(
 		}
 		m.edgeTrackerWakeInterval = time.Second * time.Duration(n.Uint64())
 	}
-	m.edgeTrackerWakeInterval = time.Second * 30
 
 	chalManager, err := m.chain.SpecChallengeManager(ctx)
 	if err != nil {
