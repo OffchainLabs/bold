bd14833079cd42f4760a560efab0aabc47c216e3
gzeon <im@gzeon.dev>
2024-03-19 18:36:07 +0000

    chore: remove licesnse diff

--- contracts/src/test-helpers/InterfaceCompatibilityTester.sol
+++ contracts/src/test-helpers/InterfaceCompatibilityTester.sol
@@ -1,5 +1,5 @@
 // Copyright 2021-2022, Offchain Labs, Inc.
-// For license information, see https://github.com/OffchainLabs/nitro-contracts/blob/main/LICENSE
+// For license information, see https://github.com/nitro/blob/master/LICENSE
 // SPDX-License-Identifier: BUSL-1.1
 
 // solhint-disable-next-line compiler-version
@@ -7,5 +7,5 @@ pragma solidity >=0.6.9 <0.9.0;
 
 import "../bridge/IBridge.sol";
 import "../bridge/IOutbox.sol";
-import "../bridge/IInboxBase.sol";
+import "../bridge/IInbox.sol";
 import "../bridge/ISequencerInbox.sol";
