e4950bc73b9da31bbe892600cd2bc36f65b28b7f
amsanghi <asanghi@offchainlabs.com>
2024-03-21 17:26:53 +0530

    clean up

--- testing/setup/rollup_stack.go
+++ testing/setup/rollup_stack.go
@@ -298,6 +298,7 @@ func ChainsWithEdgeChallengeManager(opts ...Opt) (*ChainSetup, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	chains := make([]*solimpl.AssertionChain, 0)
 	for _, acc := range accs[1:] {
 		var assertionChainBinding *rollupgen.RollupUserLogic
