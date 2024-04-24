d3e640127f27ef846d1ce0aa5e0c17594abbf430
gzeon <im@gzeon.dev>
2024-04-25 01:26:36 +0800

    feat: merge stylus ecb46c1f

--- contracts/src/state/Module.sol
+++ contracts/src/state/Module.sol
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, Offchain Labs, Inc.
+// Copyright 2021-2023, Offchain Labs, Inc.
 // For license information, see https://github.com/OffchainLabs/nitro-contracts/blob/main/LICENSE
 // SPDX-License-Identifier: BUSL-1.1
 
