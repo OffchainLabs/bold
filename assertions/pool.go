1b39678bfb204c3e29f7d2e24b37f6c41d5cd48c
Raul Jordan <raul@prysmaticlabs.com>
2024-05-24 15:28:23 -0500

    deposit into pool, wait, then trigger confirmation

--- assertions/pool.go
+++ assertions/pool.go
@@ -18,28 +18,19 @@ type useAssertionPoolArgs struct {
 func (m *Manager) useAssertionPool(
 	ctx context.Context,
 	args useAssertionPoolArgs,
-) {
+) protocol.Assertion {
 	assertionPool, err := m.getOrCreateAssertionPool(ctx, args)
 	if err != nil {
 		panic(err)
 	}
-}
+	// Max parameter as a config into how much to deposit into the pool.
+	assertionPool.depositIntoPool(ctx, args)
 
-type assertionStakingPool struct{}
+	// After this, we monitor the pool until it is ready to be posted.
+	assertionPool.waitUntilFunded(ctx, monitorPoolCreatorArgs{})
 
-// Get an assertion staking pool for the assertion we wish to post.
-func (m *Manager) getOrCreateAssertionPool(
-	ctx context.Context,
-	args useAssertionPoolArgs,
-) (*assertionStakingPool, error) {
-	var poolAddr common.Address
-	poolAddrOpt := m.checkAssertionPoolCreated(ctx, args)
-	if poolAddrOpt.IsSome() {
-		poolAddr = poolAddrOpt.Unwrap()
-	} else {
-		m.createAssertionStakingPool(ctx, args)
-	}
-	return nil, nil
+	// Then, we can trigger the posting of the assertion.
+	return assertionPool.postAssertionToPool(ctx, monitorPoolCreatorArgs{})
 }
 
 func (m *Manager) createAssertionStakingPool(ctx context.Context, args useAssertionPoolArgs) common.Address {
@@ -89,16 +80,48 @@ func (m *Manager) checkAssertionPoolCreated(
 	return option.None[common.Address]()
 }
 
+// Get an assertion staking pool for the assertion we wish to post.
+func (m *Manager) getOrCreateAssertionPool(
+	ctx context.Context,
+	args useAssertionPoolArgs,
+) (*assertionStakingPool, error) {
+	var poolAddr common.Address
+	poolAddrOpt := m.checkAssertionPoolCreated(ctx, args)
+	if poolAddrOpt.IsSome() {
+		poolAddr = poolAddrOpt.Unwrap()
+	} else {
+		poolAddr = m.createAssertionStakingPool(ctx, args)
+	}
+	return &assertionStakingPool{addr: poolAddr}, nil
+}
+
+type assertionStakingPool struct {
+	addr common.Address
+}
+
 type monitorPoolCreatorArgs struct {
 	parentAssertion protocol.AssertionHash
 	assertionHash   protocol.AssertionHash
 }
 
+func (p *assertionStakingPool) depositIntoPool(
+	ctx context.Context,
+	args useAssertionPoolArgs,
+) {
+}
+
 // Monitor any staking pools that we care about which reach the threshold.
 // Should we block until the pool threshold is reached and not post more?
 // There is only one canonical assertion branch, so yes we likely do have to wait.
-func (m *Manager) monitorAssertionPool(
+func (p *assertionStakingPool) waitUntilFunded(
 	ctx context.Context,
 	args monitorPoolCreatorArgs,
 ) {
+}
+
+func (p *assertionStakingPool) postAssertionToPool(
+	ctx context.Context,
+	args monitorPoolCreatorArgs,
+) protocol.Assertion {
+	return nil
 }
