75360d969f2bb65fe56dafc4786510d9fb0ab6b2
Raul Jordan <raul@prysmaticlabs.com>
2024-06-14 14:51:39 -0500

    onchain timer skip check

--- challenge-manager/edge-tracker/challenge_confirmation.go
+++ challenge-manager/edge-tracker/challenge_confirmation.go
@@ -3,9 +3,11 @@ package edgetracker
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	protocol "github.com/OffchainLabs/bold/chain-abstraction"
+	solimpl "github.com/OffchainLabs/bold/chain-abstraction/sol-implementation"
 	retry "github.com/OffchainLabs/bold/runtime"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -235,6 +237,14 @@ func (cc *challengeConfirmer) propageTimerUpdateToBranch(
 	tx, err := retry.UntilSucceeds(ctx, func() (*types.Transaction, error) {
 		tx, innerErr := cc.writer.MultiUpdateInheritedTimers(ctx, branch, computedLocalTimer)
 		if innerErr != nil {
+			// If are trying to update the inherited timers of a branch to a value that
+			// is less than what already exists onchain, we will receive the below error.
+			// This means we can finish early as the onchain timer is already sufficient,
+			// and our transaction reverted. We can gracefully continue if so.
+			if strings.Contains(innerErr.Error(), solimpl.ErrCachedTimeSufficient) {
+				log.Info("Onchain, cached timer for branch is already sufficient, so no need to transact", fields)
+				return nil, nil
+			}
 			log.Error("Could not transact multi-update inherited timers", fields, "err", innerErr)
 			return nil, innerErr
 		}
