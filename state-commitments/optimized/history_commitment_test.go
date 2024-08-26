70823681ba0ede4e8e8c7d796a2871a1c28a7f6f
Raul Jordan <raul@prysmaticlabs.com>
2024-08-26 17:50:58 -0500

    prefix proofs included

--- state-commitments/optimized/history_commitment_test.go
+++ state-commitments/optimized/history_commitment_test.go
@@ -1,14 +1,197 @@
 package optimized
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"fmt"
+	"math/big"
 	"testing"
 
+	"github.com/OffchainLabs/bold/containers/option"
+	l2stateprovider "github.com/OffchainLabs/bold/layer2-state-provider"
+	"github.com/OffchainLabs/bold/solgen/go/mocksgen"
 	"github.com/OffchainLabs/bold/state-commitments/history"
+	prefixproofs "github.com/OffchainLabs/bold/state-commitments/prefix-proofs"
+	statemanager "github.com/OffchainLabs/bold/testing/mocks/state-provider"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient/simulated"
 	"github.com/stretchr/testify/require"
 )
+
+func TestPrefixProofGeneration(t *testing.T) {
+	ctx := context.Background()
+	merkleTreeContract, _ := setupMerkleTreeContract(t)
+	verify := func(t *testing.T, computed *prefixProofComputation) {
+		prefixExpRaw := make([][32]byte, len(computed.prefixExpansion))
+		for i := 0; i < len(computed.prefixExpansion); i++ {
+			var r [32]byte
+			copy(r[:], computed.prefixExpansion[i][:])
+			prefixExpRaw[i] = r
+		}
+		proofRaw := make([][32]byte, len(computed.proof))
+		for i := 0; i < len(computed.proof); i++ {
+			var r [32]byte
+			copy(r[:], computed.proof[i][:])
+			proofRaw[i] = r
+		}
+		err := merkleTreeContract.VerifyPrefixProof(
+			&bind.CallOpts{},
+			computed.prefixRoot,
+			new(big.Int).SetUint64(computed.prefixTotalLeaves),
+			computed.fullRoot,
+			big.NewInt(8),
+			prefixExpRaw,
+			proofRaw,
+		)
+		require.NoError(t, err)
+		err = prefixproofs.VerifyPrefixProof(&prefixproofs.VerifyPrefixProofConfig{
+			PreRoot:      computed.prefixRoot,
+			PreSize:      computed.prefixTotalLeaves,
+			PostRoot:     computed.fullRoot,
+			PostSize:     computed.fullTreeTotalLeaves,
+			PreExpansion: computed.prefixExpansion,
+			PrefixProof:  computed.proof,
+		})
+		require.NoError(t, err)
+	}
+	tests := []struct {
+		realLength    int
+		virtualLength int
+		limit         int
+	}{
+		{1, 8, 8},
+	}
+
+	for _, tt := range tests {
+		for prefixIndex := 3; prefixIndex < tt.virtualLength; prefixIndex++ {
+			t.Run(fmt.Sprintf("real length %d, virtual %d, limit %d, prefix index %d", tt.realLength, tt.virtualLength, tt.limit, prefixIndex), func(t *testing.T) {
+				legacy := computeLegacyPrefixProof(t, ctx, tt.virtualLength, prefixIndex)
+				optimized := computeOptimizedPrefixProof(t, tt.realLength, tt.virtualLength, tt.limit, prefixIndex)
+				verify(t, legacy)
+				verify(t, optimized)
+			})
+		}
+	}
+}
+
+type prefixProofComputation struct {
+	prefixRoot          common.Hash
+	fullRoot            common.Hash
+	prefixTotalLeaves   uint64
+	fullTreeTotalLeaves uint64
+	prefixExpansion     []common.Hash
+	proof               []common.Hash
+}
+
+func computeOptimizedPrefixProof(t *testing.T, numRealHashes int, virtual int, limit int, prefixIndex int) *prefixProofComputation {
+	// Computes the prefix proof and expansion.
+	simpleHash := crypto.Keccak256Hash([]byte("foo"))
+	hashes := make([]common.Hash, numRealHashes)
+	for i := 0; i < len(hashes); i++ {
+		hashes[i] = crypto.Keccak256Hash(simpleHash[:])
+	}
+	computeVirtualSparseTreeProof(prefixIndex, hashes, virtual, 0)
+	hashes = make([]common.Hash, 8)
+	for i := 0; i < len(hashes); i++ {
+		hashes[i] = crypto.Keccak256Hash(simpleHash[:])
+	}
+	prefixExp, proof, err := computeVirtualSparseTreeProof(prefixIndex, hashes, virtual, limit)
+	require.NoError(t, err)
+	lastLeafFillers = nil
+
+	// Computes the prefix tree.
+	hashes = make([]common.Hash, prefixIndex+1)
+	for i := 0; i < len(hashes); i++ {
+		hashes[i] = crypto.Keccak256Hash(simpleHash[:])
+	}
+	computeVirtualSparseTree(hashes, prefixIndex+1, 0)
+	hashes = make([]common.Hash, prefixIndex+1)
+	for i := 0; i < len(hashes); i++ {
+		hashes[i] = crypto.Keccak256Hash(simpleHash[:])
+	}
+	prefixRoot, err := computeVirtualSparseTree(hashes, prefixIndex+1, nextPowerOf2(prefixIndex+1))
+	require.NoError(t, err)
+
+	// Computes the full tree.
+	lastLeafFillers = nil
+	hashes = make([]common.Hash, numRealHashes)
+	for i := 0; i < len(hashes); i++ {
+		hashes[i] = crypto.Keccak256Hash(simpleHash[:])
+	}
+	computeVirtualSparseTree(hashes, virtual, 0)
+	hashes = make([]common.Hash, numRealHashes)
+	for i := 0; i < len(hashes); i++ {
+		hashes[i] = crypto.Keccak256Hash(simpleHash[:])
+	}
+	fullTreeRoot, err := computeVirtualSparseTree(hashes, virtual, limit)
+	require.NoError(t, err)
+	return &prefixProofComputation{
+		prefixRoot:          prefixRoot,
+		fullRoot:            fullTreeRoot,
+		prefixTotalLeaves:   uint64(prefixIndex) + 1,
+		fullTreeTotalLeaves: uint64(virtual),
+		prefixExpansion:     prefixExp,
+		proof:               proof,
+	}
+}
+
+func computeLegacyPrefixProof(t *testing.T, ctx context.Context, numHashes int, prefixIndex int) *prefixProofComputation {
+	simpleHash := crypto.Keccak256Hash([]byte("foo"))
+	hashes := make([]common.Hash, numHashes)
+	for i := 0; i < len(hashes); i++ {
+		hashes[i] = simpleHash
+	}
+	manager, err := statemanager.NewWithMockedStateRoots(hashes)
+	require.NoError(t, err)
+
+	wasmModuleRoot := common.Hash{}
+	startMessageNumber := l2stateprovider.Height(0)
+	fromMessageNumber := l2stateprovider.Height(3)
+	req := &l2stateprovider.HistoryCommitmentRequest{
+		WasmModuleRoot:              wasmModuleRoot,
+		FromBatch:                   0,
+		ToBatch:                     10,
+		UpperChallengeOriginHeights: []l2stateprovider.Height{},
+		FromHeight:                  startMessageNumber,
+		UpToHeight:                  option.Some(l2stateprovider.Height(fromMessageNumber)),
+	}
+	loCommit, err := manager.HistoryCommitment(ctx, req)
+	require.NoError(t, err)
+
+	req.UpToHeight = option.Some(l2stateprovider.Height(numHashes - 1))
+	hiCommit, err := manager.HistoryCommitment(ctx, req)
+	require.NoError(t, err)
+
+	packedProof, err := manager.PrefixProof(ctx, req, fromMessageNumber)
+	require.NoError(t, err)
+
+	data, err := statemanager.ProofArgs.Unpack(packedProof)
+	require.NoError(t, err)
+	preExpansion, ok := data[0].([][32]byte)
+	require.Equal(t, true, ok)
+	proof, ok := data[1].([][32]byte)
+	require.Equal(t, true, ok)
+
+	preExpansionHashes := make([]common.Hash, len(preExpansion))
+	for i := 0; i < len(preExpansion); i++ {
+		preExpansionHashes[i] = preExpansion[i]
+	}
+	prefixProof := make([]common.Hash, len(proof))
+	for i := 0; i < len(proof); i++ {
+		prefixProof[i] = proof[i]
+	}
+	return &prefixProofComputation{
+		prefixRoot:          loCommit.Merkle,
+		fullRoot:            hiCommit.Merkle,
+		prefixTotalLeaves:   uint64(prefixIndex) + 1,
+		fullTreeTotalLeaves: uint64(numHashes),
+		prefixExpansion:     preExpansionHashes,
+		proof:               prefixProof,
+	}
+}
 
 func TestLegacyVsOptimized(t *testing.T) {
 	end := 1 << 9
@@ -249,4 +432,62 @@ func BenchmarkMaximumDepthHistoryCommitment(b *testing.B) {
 		}
 		computeVirtualSparseTree(hashedLeaves, 1<<26, 1<<26)
 	}
+}
+
+func setupMerkleTreeContract(t testing.TB) (*mocksgen.MerkleTreeAccess, *simulated.Backend) {
+	numChains := uint64(1)
+	accs, backend := setupAccounts(t, numChains)
+	_, _, merkleTreeContract, err := mocksgen.DeployMerkleTreeAccess(accs[0].txOpts, backend.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+	backend.Commit()
+	return merkleTreeContract, backend
+}
+
+// Represents a test EOA account in the simulated backend,
+type testAccount struct {
+	accountAddr common.Address
+	txOpts      *bind.TransactOpts
+}
+
+func setupAccounts(t testing.TB, numAccounts uint64) ([]*testAccount, *simulated.Backend) {
+	genesis := make(core.GenesisAlloc)
+	gasLimit := uint64(100000000)
+
+	accs := make([]*testAccount, numAccounts)
+	for i := uint64(0); i < numAccounts; i++ {
+		privKey, err := crypto.GenerateKey()
+		if err != nil {
+			t.Fatal(err)
+		}
+		pubKeyECDSA, ok := privKey.Public().(*ecdsa.PublicKey)
+		if !ok {
+			t.Fatal("not ok")
+		}
+
+		// Strip off the 0x and the first 2 characters 04 which is always the
+		// EC prefix and is not required.
+		publicKeyBytes := crypto.FromECDSAPub(pubKeyECDSA)[4:]
+		var pubKey = make([]byte, 48)
+		copy(pubKey, publicKeyBytes)
+
+		addr := crypto.PubkeyToAddress(privKey.PublicKey)
+		chainID := big.NewInt(1337)
+		txOpts, err := bind.NewKeyedTransactorWithChainID(privKey, chainID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		startingBalance, _ := new(big.Int).SetString(
+			"100000000000000000000000000000000000000",
+			10,
+		)
+		genesis[addr] = core.GenesisAccount{Balance: startingBalance}
+		accs[i] = &testAccount{
+			accountAddr: addr,
+			txOpts:      txOpts,
+		}
+	}
+	backend := simulated.NewBackend(genesis, simulated.WithBlockGasLimit(gasLimit))
+	return accs, backend
 }
