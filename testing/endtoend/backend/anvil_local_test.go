af27b1407506a8ee3d5f8c6c849a590a8be9e26d
Raul Jordan <raul@prysmaticlabs.com>
2024-11-21 10:13:37 -0600

    rm flakey anvil test as anvil is not even used now, maybe we should remove later

--- testing/endtoend/backend/anvil_local_test.go
+++ testing/endtoend/backend/anvil_local_test.go
@@ -6,10 +6,6 @@ package backend
 import (
 	"context"
 	"testing"
-	"time"
-
-	retry "github.com/offchainlabs/bold/runtime"
-	"github.com/stretchr/testify/require"
 )
 
 func TestLocalAnvilLoadAccounts(t *testing.T) {
@@ -22,34 +18,5 @@ func TestLocalAnvilLoadAccounts(t *testing.T) {
 	}
 	if len(a.accounts) == 0 {
 		t.Error("No accounts generated")
-	}
-}
-
-func TestLocalAnvilStarts(t *testing.T) {
-	t.Skip("Flakey in CI")
-	ctx, cancel := context.WithTimeout(context.TODO(), 30*time.Second)
-	defer cancel()
-	a, err := NewAnvilLocal(ctx)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err2 := a.Start(ctx); err2 != nil {
-		t.Fatal(err2)
-	}
-	_, err = retry.UntilSucceeds(ctx, func() (bool, error) {
-		if _, err2 := a.DeployRollup(ctx); err2 != nil {
-			return false, err2
-		}
-		return true, nil
-	})
-	require.NoError(t, err)
-
-	// There should be at least 100 blocks
-	bn, err2 := a.Client().HeaderNumberUint64(ctx, nil)
-	if err2 != nil {
-		t.Fatal(err2)
-	}
-	if bn < 100 {
-		t.Errorf("Expected at least 100 blocks at start, but got only %d", bn)
 	}
 }
